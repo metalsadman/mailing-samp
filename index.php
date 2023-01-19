0fbaf0a3d975f5bd781aac78f6f575703cb3edcf
Aldrin Marquez <aldringm123@gmail.com>
2023-01-19 09:45:31 +0800

    up

--- index.php
+++ index.php
@@ -10,45 +10,55 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     
     <title>Mailing Address</title>
+    <style>
+        body {background-color: #E7E9EB;}
+    </style>
 </head>
 <body>
     <div class="container d-flex justify-content-center">
-        <form class="col-md-6 m-3" id="address-form">
-            <div class="form-group">
-                <label for="street">Street Address</label>
-                <input type="text" class="form-control" id="street" name="street">
-            </div>
-            <div class="form-group">
-                <label for="city">City</label>
-                <input type="text" class="form-control" id="city" name="city">
-            </div>
-            <div class="form-group">
-                <label for="state">State</label>
-                <select class="form-control" id="state" name="state">
-                    <option value=""></option>
-                    <?php
-                    require('states.php');
-                    foreach($states as $key => $value) {
-                        echo "<option value='$key'>$value</option>";
-                    }
-                    ?>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="zip">Zip Code</label>
-                <input type="text" class="form-control" id="zip" name="zip">
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" value="true" id="standardized" name="standardized">
-                <label class="form-check-label" for="standardized">
-                    Standardized Address
-                </label>
-            </div>
-            <button type="submit" class="btn btn-primary" id="submitBtn">Submit</button>
-            <div class="alert alert-danger my-3" role="alert" id="error" style="display:none;"></div>
-            <div class="alert alert-success my-3" role="alert" id="success" style="display:none;"></div>
-        </form>
-        
+        <div class="card shadow-sm" style="width: 40rem;">
+            <div class="card-body">
+            <form class="m-1" id="address-form">
+                <div class="form-group">
+                    <label for="address1">Address Line 1</label>
+                    <input type="text" class="form-control" id="address1" name="address1">
+                </div>
+                <div class="form-group">
+                    <label for="address2">Address Line 2</label>
+                    <input type="text" class="form-control" id="address2" name="address2">
+                </div>
+                <div class="form-group">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" id="city" name="city">
+                </div>
+                <div class="form-group">
+                    <label for="state">State</label>
+                    <select class="form-control" id="state" name="state">
+                        <option value=""></option>
+                        <?php
+                        require('states.php');
+                        foreach($states as $key => $value) {
+                            echo "<option value='$key'>$value</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="zip">Zip Code</label>
+                    <input type="text" class="form-control" id="zip" name="zip">
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="true" id="standardized" name="standardized">
+                    <label class="form-check-label" for="standardized">
+                        Standardized Address
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary" id="submitBtn">Submit</button>
+                <div class="alert alert-danger my-3" role="alert" id="error" style="display:none;"></div>
+                <div class="alert alert-success my-3" role="alert" id="success" style="display:none;"></div>
+            </form>
+            </div>            
+        </div>
     </div>
     <script>
         // Get the form element
